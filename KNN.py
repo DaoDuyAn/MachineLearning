19b625fc064caddef4c890efcd4eebb9768b9418
Đào Duy An <84566480+DaoDuyAn@users.noreply.github.com>
2023-05-07 16:41:29 +0700

    Add files via upload

---
+++ KNN.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+
+# Importing the dataset
+dataset = pd.read_csv('flags.csv')
+X = dataset.iloc[:, 7:26].values
+y = dataset.iloc[:, 29].values
+
+# Splitting the dataset into the Training set and Test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+# Feature Scaling
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+# fitting KNN classifier to the training set
+classifier = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2)
+classifier.fit(X_train, y_train)
+
+# predict test set result
+y_pred = classifier.predict(X_test)
+# print(y_test)
+# print(y_pred)
+print(accuracy_score(y_test, y_pred, normalize=False))
+print(round(accuracy_score(y_test, y_pred)*100, 2))
